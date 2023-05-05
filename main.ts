93d16c6dc7cd19d9f3e9db47e9ec24f9b83cead0
Alex Sorokin <alex@mx3d.com>
2023-05-05 12:23:45 +0200

    split into several functions

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
 const isObject = (obj: unknown): boolean => obj && typeof obj === 'object' && !Array.isArray(obj);
 
-const transformStrToCamel = (str: string, divider: string = '_'): string => {
+const capitalizeFirstLetter = (str: string): string => str[0].toUpperCase() + str.slice(1);
+
+const transformStrToCamelCase = (str: string, divider: string = '_'): string => {
     const [first, ...rest] = str.split(divider);
-    return [first, ...rest.map((item) => item[0].toUpperCase() + item.slice(1))].join('');
+    return [first, ...rest.map((word) => capitalizeFirstLetter(word))].join('');
 };
 
-const keysToCamelCase = <T>(obj: T): T => {
+export const keysToCamelCase = <T>(obj: T): T => {
     if (!isObject(obj)) return obj;
     return Object.entries(obj).reduce((acc, [key, value]: [string, T | unknown]) => {
         const lowerKey = key.toLowerCase();
-        const snakeToKamel = transformStrToCamel(lowerKey, '_');
-        const transformedKey = transformStrToCamel(snakeToKamel, '-');
+        const snakeToKamel = transformStrToCamelCase(lowerKey, '_');
+        const transformedKey = transformStrToCamelCase(snakeToKamel, '-');
         return { ...acc, [transformedKey]: keysToCamelCase(value) };
     }, {} as T);
 };
